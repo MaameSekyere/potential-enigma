4f46b31038c8ced04ce787a57372787e32c41806
MaameSekyere <maameant@gmail.com>
2022-04-24 22:15:39 -0400

    generated readme with node

--- README.md
+++ README.md
@@ -1,3 +1,32 @@
-# Professional README Generator Starter Code
 
-[How to create a Professional README](https://coding-boot-camp.github.io/full-stack/github/professional-readme-guide)
+# Table of Contents
+*[Title] (#Title)
+*[Description] (#Description)
+*[Installation] (#Installation)
+*[Usage](#Usage)
+*[License] (#License)
+*[Contribution] (#Contribution)
+*[Tests] (#Tests)
+*[Links] (#Links)
+*[Email] (#Email)
+
+## Title
+Professional README generator  
+## Description
+A readme generator that can quickly create a professional readme for a new project
+## Installation
+Node and inquirer installations
+## Usage
+To create professional readme for new projects
+## License
+Apache
+## Contribution
+Node modules, Tutor services
+## Tests
+Ei duo eius putant, lorem commodo at vim. Nihil prompta ne eam, his erat saepe te. Audiam conclusionemque ex vis. Ut regione petentium moderatius vim, saepe mentitum his ex, fugit graece id vis.
+## Links
+<a href= "https://github.com/maamesekyere"> Github</a>
+## maameant@gmail.com 
+<a href="mailto:maameant@gmail.com"> Email address</a>
+        
+        
--- index.js
+++ index.js
@@ -1,8 +1,170 @@
 // TODO: Include packages needed for this application
+const fs = require("fs");
+const inquirer = require("inquirer");
 
 // TODO: Create an array of questions for user input
-//const questions = [];
+const promptQuestions = () => {
+  console.log(promptQuestions);
+  return inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "Title",
+        message: "What is your project name? (Required)",
+        validate: (nameInput) => {
+          if (nameInput) {
+            return true;
+          } else {
+            return "a value is required!";
+          }
+        },
+      },
+      {
+        type: "input",
+        name: "Description",
+        message: "Describe your project? (Required)",
+        validate: (descriptionInput) => {
+          if (descriptionInput) {
+            return true;
+          } else {
+            return "a value is required!";
+          }
+        },
+      },
 
+      {
+        type: "input",
+        name: "Installation",
+        message: "What installations did you use? (Required)",
+        validate: (installInput) => {
+          if (installInput) {
+            return true;
+          } else {
+            return "a value is required!";
+          }
+        },
+      },
+      {
+        type: "input",
+        name: "Usage",
+        message: "How is your project used? (Required)",
+        validate: (usageInput) => {
+          if (usageInput) {
+            return true;
+          } else {
+            return "a value is required!";
+          }
+        },
+      },
+      {
+        type: "checkbox",
+        name: "License",
+        message: "What license did you use? (Check all that apply)",
+        choices: ["Apache", "GPL", "MIT", "Boost", "BSD", "N/A"],
+      },
+      {
+        type: "input",
+        name: "Contribution",
+        message: "Who contributed to your project? (Required)",
+        validate: (contributeInput) => {
+          if (contributeInput) {
+            return true;
+          } else {
+            return "a value is required!";
+          }
+        },
+      },
+      {
+        type: "input",
+        name: "Tests",
+        message: "How do you test your project? (Required)",
+        validate: (testInput) => {
+          if (testInput) {
+            return true;
+          } else {
+            return "a value is required!";
+          }
+        },
+      },
+      {
+        type: "input",
+        name: "Links",
+        message: "Enter the github link to your project (Required)",
+        validate: (linkInput) => {
+          if (linkInput) {
+            return true;
+          } else {
+            return "a value is required!";
+          }
+        },
+      },
+      {
+        type: "input",
+        name: "Email",
+        message: "What is your email address? (Required)",
+        validate: (emailInput) => {
+          if (emailInput) {
+            return true;
+          } else {
+            return "a value is required!";
+          }
+        },
+      },
+    ])
+    .then(function ({
+      Title,
+      Description,
+      Installation,
+      Usage,
+      License,
+      Contribution,
+      Tests,
+      Links,
+      Email,
+    }) {
+      fs.writeFile(
+        "./README.md",
+        `
+# Table of Contents
+*[Title] (#Title)
+*[Description] (#Description)
+*[Installation] (#Installation)
+*[Usage](#Usage)
+*[License] (#License)
+*[Contribution] (#Contribution)
+*[Tests] (#Tests)
+*[Links] (#Links)
+*[Email] (#Email)
+
+## Title
+${Title}  
+## Description
+${Description}
+## Installation
+${Installation}
+## Usage
+${Usage}
+## License
+${License}
+## Contribution
+${Contribution}
+## Tests
+${Tests}
+## Links
+<a href= ${Links}> Github</a>
+## ${Email} 
+<a href="mailto:${Email}"> Email address</a>
+        
+        `,
+        (err) => {
+          if (err) throw new Error(err);
+          console.log("Readme file created!");
+        }
+      );
+    });
+};
+
+promptQuestions();
 // TODO: Create a function to write README file
 //function writeToFile(fileName, data) {}
 
@@ -11,5 +173,3 @@
 
 // Function call to initialize app
 //init();
-
-console.log("Hello Node!");
